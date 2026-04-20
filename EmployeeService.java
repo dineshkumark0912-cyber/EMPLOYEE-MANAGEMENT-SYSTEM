4be320a31053a7a9761a92edda52107b3c826f2f
dineshkumark0912-cyber <dineshkumark0912@gmail.com>
2026-04-20 08:56:51 +0530

    Add files via upload
    THIS PROGRAM WAS DONE USING ECLIPSE IDE

---
+++ EmployeeService.java
@@ -0,0 +1,48 @@
+package EMS;
+import java.util.*;
+
+public class EmployeeService {
+	private List<Employee>empList = new ArrayList();
+	public void addEmployee(Employee emp) {
+		empList.add(emp);
+		System.out.println("Employee Added!");
+		
+	}
+	public void viewEmployees() {
+		for (Employee e:empList) {
+			e.display();
+		}
+	}
+	public Employee searchEmployee(int id) {
+		for (Employee e: empList) {
+			if (e.getId() == id) {
+				return e;
+			}
+		}
+		return null;
+	}
+	public void deleteEmployee(int id) {
+		Employee emp = searchEmployee(id);
+		if (emp != null) {
+			empList.remove(emp);
+			System.out.println("employee Deleted");
+		}
+		else {
+			System.out.println("EmployeeNot Found!");
+		}
+	}
+	public void updateEmployee(int id,String name,double salary) {
+		Employee emp = searchEmployee(id);
+		if(emp !=null) {
+			emp.setName(name);
+			emp.setSalary(salary);
+			System.out.println("Employee Updated");
+			
+		}
+		else {
+			System.out.println("Employee Not Found!");
+		}
+	}
+
+	}
+
